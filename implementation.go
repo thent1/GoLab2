16f5a4237179140a86835ce235b45a2e38be1552
Sashko Vysokovskiy <thettboy11@gmail.com>
2024-03-21 16:31:50 +0200

    fix signs like ! ~ ?

--- implementation.go
+++ implementation.go
@@ -2,47 +2,49 @@ package lab2
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
 
-// PostfixToInfix returns postfix expression converted into infix expression
+// isOperator проверяет, является ли символ оператором
+func isOperator(c string) bool {
+	return strings.ContainsAny(c, "+-*/^")
+}
+
+// isValidOperand проверяет, является ли строка допустимым операндом (числом)
+func isValidOperand(c string) bool {
+	_, err := strconv.Atoi(c)
+	return err == nil
+}
+
+// PostfixToInfix возвращает постфиксное выражение, преобразованное в инфиксное
 func PostfixToInfix(expression string) (string, error) {
-	// Стек для збереження проміжних результатів
 	stack := []string{}
-
-	// Функция isOperator проверяет, является ли символ оператором
-	isOperator := func(char string) bool {
-		operators := map[string]bool{"+": true, "-": true, "*": true, "/": true, "^": true}
-		_, ok := operators[char]
-		return ok
-	}
-
-	// Разделяем выражение на операнды и операторы по пробелам
 	tokens := strings.Split(expression, " ")
 
-	// Проходим по каждому токену в выражении
 	for _, token := range tokens {
-		// Если токен - это оператор, извлекаем два последних операнда из стека
-		if isOperator(token) {
-			if len(stack) < 2 {
-				return "", fmt.Errorf("неправильний вираз: не вистачає операндів для операції")
-			}
-			operand2 := stack[len(stack)-1]
-			stack = stack[:len(stack)-1]
-			operand1 := stack[len(stack)-1]
-			stack = stack[:len(stack)-1]
+		if isOperator(token) || isValidOperand(token) {
+			if isOperator(token) {
+				if len(stack) < 2 {
+					return "", fmt.Errorf("неправильный ввод: недостаточно операндов для оператора")
+				}
+				operand2 := stack[len(stack)-1]
+				stack = stack[:len(stack)-1]
+				operand1 := stack[len(stack)-1]
+				stack = stack[:len(stack)-1]
 
-			// Формируем инфиксное выражение и добавляем его в стек
-			infixExpression := fmt.Sprintf("(%s %s %s)", operand1, token, operand2)
-			stack = append(stack, infixExpression)
-		} else { // Если токен - это операнд, добавляем его в стек
-			stack = append(stack, token)
+				infixExpression := fmt.Sprintf("(%s %s %s)", operand1, token, operand2)
+				stack = append(stack, infixExpression)
+			} else {
+				stack = append(stack, token)
+			}
+		} else {
+			return "", fmt.Errorf("неправильный ввод: недопустимый символ %s", token)
 		}
 	}
 
-	// Проверяем наличие лишних операндов
 	if len(stack) != 1 {
-		return "", fmt.Errorf("неправильний вираз: лишні операнди")
+		return "", fmt.Errorf("неправильный ввод: лишние операнды")
 	}
 
 	return stack[0], nil
